41919a10f68c52bfb5724643ede96474bead3154
bryan <bryammd7@hotmail.com>
2023-11-13 16:01:28 +0100

    feat(sendData): add location funcion

--- client/src/Services.ts
+++ client/src/Services.ts
@@ -1,3 +1,8 @@
+interface GeoLocationData {
+  latitude?: number;
+  longitude?: number;
+}
+
 export const services = {
   getData: async(url: string)=>{
     try {
@@ -52,6 +57,21 @@ export const services = {
 
     } catch (error) {
       console.log(error)
+    }
+  },
+  postDataLocation : async (data: GeoLocationData) => {
+    try {
+      const response = await fetch('http://localhost:8080/api/location',{
+        method: 'POST',
+        headers: {'content-type':'application/json'},
+        body: JSON.stringify(data)
+      })
+      const location = await response.json();
+      console.log(data)
+      return location
+  ;
+    } catch (error) {
+      console.error('Error al enviar los datos:', error);
     }
   }
 }
