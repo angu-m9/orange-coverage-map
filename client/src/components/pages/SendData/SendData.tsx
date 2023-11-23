9962319f2ac40dcdba46bef046db04bafff41f2c
angu-m9 <n.angulopop@gmail.com>
2023-11-23 20:42:46 +0100

    fix(client): replace watchPosition with getCurrentPosition in SendData component to reduce latency

--- client/src/components/pages/SendData/SendData.tsx
+++ client/src/components/pages/SendData/SendData.tsx
@@ -1,50 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Header from "../../templates/Header/Header";
 import { useNetwork } from "../../../hooks/useNetwork";
-import { useNavigate } from "react-router-dom";
 import { services } from "../../../services/services";
 import ModalSucces from "../../templates/ModalSucces/ModalSucces";
 import ModalError from "../../templates/ModalError/ModalError";
 
 const SendData = (): React.JSX.Element => {
-  const [ModalErr, setModalError] = useState(false)
-  const [ModalSuccess, setModalSuccess] = useState(false)
-  const [change, setChange] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [index, setIndex] = useState(0);
+  const [ModalErr, setModalError] = useState(false);
+  const [ModalSuccess, setModalSuccess] = useState(false);
   const networkInfo = useNetwork();
-  const userUuid = localStorage.getItem('userUuid'); // Recuperamos el UUID del localStorage
-  
+  const userUuid = localStorage.getItem('userUuid');
 
-
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    const watchId = navigator.geolocation.watchPosition(
-      () => {
-        console.log('compartiendo ubicacion');
-      },
-      () => {
-        navigate('/permission');
-      }
-    );
-
-    return () => {
-      navigator.geolocation.clearWatch(watchId);
-    };
-  }, [navigate]);
-
-
-
-  const handleClose = () => {
-    setChange(false);
-    setErrorMessage('');
-  };
-
-
-  const sendLocation =async()=>{
+  const sendLocation = () => {
     navigator.geolocation.getCurrentPosition(async (position) => {
-
       const geoLocationData = {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
@@ -54,53 +22,48 @@ const SendData = (): React.JSX.Element => {
         ...geoLocationData,
         ...networkInfo,
         network: networkInfo.effectiveType,
-        userUuid // Enviamos el UUID junto con los datos de la red
+        userUuid
       };
-
-      console.log(combinedData);
 
       try {
         const postSuccess = await services.postDataList(combinedData);
 
-        setChange(true);
-
-      if (postSuccess) {
-        setModalSuccess(true)
-        setTimeout(()=>{
-          setModalSuccess(false)
-        },3000)
-      } else {
-        setModalError(true)
-        setTimeout(()=>{
-          setModalError(false)
-        },3000)
-        
-      }
+        if (postSuccess) {
+          setModalSuccess(true);
+          setTimeout(() => {
+            setModalSuccess(false);
+          }, 3000);
+        } else {
+          setModalError(true);
+          setTimeout(() => {
+            setModalError(false);
+          }, 3000);
+        }
       } catch (error) {
         console.error('Error al enviar los datos de red:', error);
-        setErrorMessage('Error al enviar los datos. Por favor, intente de nuevo.');
+        setModalError(true);
+        setTimeout(() => {
+          setModalError(false);
+        }, 3000);
       }
     }, (error) => {
-      setErrorMessage('Error al obtener la geolocalización. Por favor, intente de nuevo.');
+      console.error('Error al obtener la geolocalización:', error);
+      setModalError(true);
+      setTimeout(() => {
+        setModalError(false);
+      }, 3000);
     });
   };
-
-  
-  
 
   return (
     <>
       <Header title="Enviar Datos" />
       <div className="container py-4 px-3 mx-auto b-1 text-center mt-3">
-        <div
-          className="d-flex flex-column align-items-center justify-content-center container-sendData"
-        >
+        <div className="d-flex flex-column align-items-center justify-content-center container-sendData">
           <img src="src/assets/images/send-data.svg" alt="send-data" />
-
           <p className="w-80">
             Comparte tu calidad de red junto a tu ubicación.
           </p>
-
           <button
             type="button"
             className="btn btn-primary button__send-data"
@@ -109,10 +72,8 @@ const SendData = (): React.JSX.Element => {
             Enviar
           </button>
         </div>
-
         <ModalSucces display={ModalSuccess}/>
-        <ModalError display={ModalErr} />
-
+        <ModalError display={ModalErr}/>
       </div>
     </>
   );
