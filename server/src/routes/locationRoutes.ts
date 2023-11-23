9ab97d03f0b90f8dce8afa06008f5bd34b7c908d
VirginiaSantana <virsantana@gmail.com>
2023-11-23 09:15:17 +0100

    feat(middlewares): create middlewares

--- server/src/routes/locationRoutes.ts
+++ server/src/routes/locationRoutes.ts
@@ -1,4 +1,14 @@
-/*import express from 'express';
+// import express from 'express';
+// import { postLocation } from '../controllers/locationController';
+
+// const locationRouter = express.Router();
+
+// locationRouter.post('/', postLocation);
+
+// export default locationRouter;
+// locationRoutes.ts
+
+import express from 'express';
 import { postLocation } from '../controllers/locationController';
 import { getLocations } from '../controllers/locationController';
 const locationRouter = express.Router();
