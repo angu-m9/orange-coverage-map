c3bd356b89d8f681ab510e1cb57498273186ba4f
bryan <bryammd7@hotmail.com>
2023-11-28 09:10:44 +0100

    feat(datalist): pagination

--- client/src/components/pages/DataList/DataList.tsx
+++ client/src/components/pages/DataList/DataList.tsx
@@ -1,23 +1,32 @@
 import CsvDownloader from "react-csv-downloader";
 import { useLoaderData } from "react-router";
 import HeaderAdmin from "../../templates/HeaderAdmin/HeaderAdmin";
-import React, { Key, useState } from "react";
+import React, { Key, useState, useEffect } from "react";
 import './dataList.style.css'
 import { ResponseListInterface } from "../../../services/service.module";
 
 const DataList = (): React.JSX.Element => {
   const { response } = useLoaderData() as ResponseListInterface;
+  const [formattedResponse, setFormattedResponse] = useState([]);
 
-
-  response.forEach((row: { created_at: string | number | Date }) => {
-    row.created_at = new Date(row.created_at).toLocaleDateString();
-  });
+  useEffect(() => {
+    if (Array.isArray(response) && response.length > 0 && formattedResponse.length === 0) {
+      const formattedData = response.map((row) => {
+        const date = new Date(row.created_at);
+        return {
+          ...row,
+          created_at: isNaN(date.getTime()) ? 'Fecha inválida' : date.toLocaleDateString(),
+        };
+      });
+      setFormattedResponse(formattedData);
+    }
+  }, [response]);
 
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = response.slice(indexOfFirstItem, indexOfLastItem);
+  const currentItems = formattedResponse.slice(indexOfFirstItem, indexOfLastItem);
 
   const columns = [
     { id: "id", displayName: "ID" },
@@ -30,7 +39,7 @@ const DataList = (): React.JSX.Element => {
   ];
 
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(response.length / itemsPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(formattedResponse.length / itemsPerPage); i++) {
     pageNumbers.push(i);
   }
 
@@ -38,82 +47,70 @@ const DataList = (): React.JSX.Element => {
     <>
       <HeaderAdmin mapCoverage={""} dataList={"active"} />
       <div className="container-black">
-      <div className="container-black container py-4 px-3 mx-auto b-1 text-center">
-        <div className="container__data-list">
-          <table className="table"> 
-            <thead>
-              <tr >
-                {columns.map((col) => (
-                  <th id="table-color" key={col.id} scope="col">
-                    {col.displayName}
-                  </th>
-                ))}
-              </tr>
-            </thead>
+        <div className="container-black container py-4 px-3 mx-auto b-1 text-center">
+          <div className="container__data-list">
+            <table className="table"> 
+              <thead>
+                <tr>
+                  {columns.map((col) => (
+                    <th id="table-color" key={col.id} scope="col">
+                      {col.displayName}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
 
-            <tbody>
-              {currentItems.map(
-                (row: { id: Key; created_at: string | number | Date }) => (
+              <tbody>
+                {currentItems.map((row: { id: Key; created_at: string | number | Date }) => (
                   <tr key={row.id}>
                     {columns.map((col) => (
                       <td id="table-color" key={col.id}>{row[col.id]}</td>
                     ))}
                   </tr>
-                )
-              )}
-            </tbody>
-          </table>
-        </div>
+                ))}
+              </tbody>
+            </table>
+          </div>
 
-        <div className="d-flex justify-content-center">
-          <nav aria-label="Page navigation example ">
-            <ul className="pagination">
-              <li className="page-item">
-                <a
-                  className="page-link"
-                  onClick={() =>
-                    setCurrentPage((prev) => (prev > 1 ? prev - 1 : prev))
-                  }
-                >
-                  
-                </a>
-              </li>
-              {pageNumbers.map((number) => (
-                <li key={number} className="page-item">
+          <div className="d-flex justify-content-center">
+            <nav aria-label="Page navigation example ">
+              <ul className="pagination">
+                <li className="page-item">
                   <a
-                    href="#"
-                    className={`page-link ${number === currentPage ? 'active' : ''}`}
-                    onClick={() => setCurrentPage(number)}
-                  >
-                    {number}
-                  </a>
+                    className="page-link"
+                    onClick={() => setCurrentPage((prev) => (prev > 1 ? prev - 1 : prev))}
+                  ></a>
                 </li>
-              ))}
-              <li className="page-item">
-                <a
-                  className="page-link"
-                  onClick={() =>
-                    setCurrentPage((prev) =>
-                      prev < pageNumbers.length ? prev + 1 : prev
-                    )
-                  }
-                >
-                  
-                </a>
-              </li>
-            </ul>
-          </nav>
+                {pageNumbers.map((number) => (
+                  <li key={number} className="page-item">
+                    <a
+                      href="#"
+                      className={`page-link ${number === currentPage ? 'active' : ''}`}
+                      onClick={() => setCurrentPage(number)}
+                    >
+                      {number}
+                    </a>
+                  </li>
+                ))}
+                <li className="page-item">
+                  <a
+                    className="page-link"
+                    onClick={() => setCurrentPage((prev) => (prev < pageNumbers.length ? prev + 1 : prev))}
+                  ></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <CsvDownloader
+            filename="datos"
+            extension=".csv"
+            columns={columns}
+            datas={formattedResponse}
+            text="EXPORT"
+          >
+            <button className="btn btn-primary m-4 w-25">Exportar</button>
+          </CsvDownloader>
         </div>
-        <CsvDownloader
-          filename="datos"
-          extension=".csv"
-          columns={columns}
-          datas={response}
-          text="EXPORT"
-        >
-          <button className="btn btn-primary m-4 w-25">Exportar</button>
-        </CsvDownloader>
-      </div>
       </div>
     </>
   );
--- server/src/testing/app.test.ts
+++ server/src/testing/app.test.ts
@@ -4,9 +4,9 @@ import { app } from '../config/server';
 
 describe('App Tests', () => {
   test('GET', async () => {
-    const response = await request(app).get('/');
+    const response = await request(app).get('/').send();
     expect(response.status).toBe(200);
-
+    expect(response.headers['content-type']).toContain('json')
   });
 });
 
